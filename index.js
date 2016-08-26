ccffb009865f244766f92aa8502b7bc635ede43b
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-08-26 15:14:17 +0200

    last changes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var tokenLibrary = require('./variables');
 var _ = require('underscore');
 var nextStep = false;
 
-var response = {};
+var respuesta = {};
+var screens;
+var directURL = false;
 
 var terms = ['image', 'images', 'picture', 'pictures', 'flickr', 'youtube', 'video', 'play', 'busca', 'google', 'web', 'search'];
 
@@ -24,41 +26,48 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-    if(message.user && message.user != 'U242HLVFU'){ 
-          var checkResponse =  parseInt(message.text); 
-          response.user = rtm.dataStore.getUserById(message.user);
-          response.dm = rtm.dataStore.getDMByName(response.user.name);
-          response.texto = message.text.split(" ");
-          if(!isNaN(checkResponse) && nextStep && checkResponse < 3) {
-                console.log('mensaje original: ', checkResponse, response.query, response.querySource );
-                if(checkResponse == 1) var device = 'Showroom.1';
-                else if(checkResponse == 2) var device = 'tv.1';
-                search.search(response.query, response.querySource, function(err, content){
-                       content.screen = device; 
-                       console.log('contenido a enviar: ', content); 
-                       request.post({
-                            url: tokenLibrary.baseUrl+'/content',
-                            json: content
-                       });
-                });
-                rtm.sendMessage('enviando '+ response.query +' a la pantalla '+ device, response.dm.id);  
+    if(message.user && message.user != 'U242HLVFU'){
+          //console.log('mensaje: ', message);
+          //else directURL = false;
+          var checkrespuesta =  parseInt(message.text); 
+          respuesta.user = rtm.dataStore.getUserById(message.user);
+          respuesta.dm = rtm.dataStore.getDMByName(respuesta.user.name);
+          respuesta.texto = message.text.split(" ");
+          if(!isNaN(checkrespuesta) && nextStep && checkrespuesta < screens.length) {
+                    console.log('entra en search');
+                    search.search(respuesta.query, respuesta.querySource, function(err, content){
+                           content.screen = screens[checkrespuesta].name; 
+                           console.log('contenido a enviar: ', content); 
+                           request.post({
+                                url: tokenLibrary.baseUrl+'/content',
+                                json: content
+                           });
+                    });  
+                rtm.sendMessage('enviando '+ respuesta.query +' a la pantalla '+ screens[checkrespuesta].name, respuesta.dm.id);  
           }
-          else if( isNaN(checkResponse)){
-                response.querySource = response.texto[0];
-                if(_.indexOf(terms,response.querySource) != -1){
+          else if( isNaN(checkrespuesta)){
+                if(message.text.charAt(0) === '<') { respuesta.querySource = 'web' }
+                else respuesta.querySource = respuesta.texto[0];
+                if(_.indexOf(terms,respuesta.querySource) != -1 || directURL != false){
+                    if(message.text.charAt(0) === '<') respuesta.query = message.text;
+                    else respuesta.query = respuesta.query = _.without(respuesta.texto, respuesta.texto[0]).join(' ');
                     nextStep = true;
-                    console.log(message.text);
-                    response.query = _.without(response.texto, response.texto[0]).join(' ');
-                        rtm.sendMessage('En que dispositivo lo quieres mostrar? _*1.*_:    showroom, _*2.*_:    tv sala 1: ', response.dm.id, function (err, msg) {
-                         
-                    });
+                    console.log('respuesta: ', respuesta);
+                    rtm.sendMessage('En que dispositivo lo quieres mostrar?:', respuesta.dm.id, function (err, msg) {});
+                    request.get( tokenLibrary.baseUrl+'/screens', function(error, resp, body) {
+                            screens = JSON.parse(body);
+                            if(screens.length < 1) respuesta.tell("no tienes teles, eres pobre");
+                            screens.map(function(item, i){ 
+                                rtm.sendMessage( '>*' + i + '.:*   ' + item.name, respuesta.dm.id, function (err, msg) {});
+                            });
+                      })
                 }
                 else {
-                        rtm.sendMessage(">utiliza *'youtube'*, *'video'* o *'play'* seguido del termino que quieras encontrar para reproducir un _*video*_.", response.dm.id);
-                        rtm.sendMessage(">utiliza *'image'*, *'images'*, *'picture'*, *'pictures'* o *'flickr'* seguido del termino que quieras encontrar para mostrar una _*imagen*_.", response.dm.id);
-                        rtm.sendMessage(">y por ultimo utiliza *'busca'*, *'google'*, *'web'* o *'search'* seguido del termino que quieras encontrar para mostrar la _*pagina web*_ que desees.", response.dm.id); 
+                        rtm.sendMessage(">utiliza *'youtube'*, *'video'* o *'play'* seguido del termino que quieras encontrar para reproducir un _*video*_.", respuesta.dm.id);
+                        rtm.sendMessage(">utiliza *'image'*, *'images'*, *'picture'*, *'pictures'* o *'flickr'* seguido del termino que quieras encontrar para mostrar una _*imagen*_.", respuesta.dm.id);
+                        rtm.sendMessage(">y por ultimo utiliza *'busca'*, *'google'*, *'web'* o *'search'* seguido del termino que quieras encontrar para mostrar la _*pagina web*_ que desees.", respuesta.dm.id); 
                 }
           }
-          else { rtm.sendMessage('tienes que decirme que quieres que busque y que hago con ello ' + response.user.name + '!', response.dm.id); };
+          else { rtm.sendMessage('tienes que decirme que quieres que busque y que hago con ello ' + respuesta.user.name + '!', respuesta.dm.id); };
     }
 });
